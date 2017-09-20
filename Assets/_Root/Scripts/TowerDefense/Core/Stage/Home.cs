2d447c4f761d26035ffa0da5558675bcf949f519
artem.korotenko <artem.korotenko@tatemgames.com>
2017-09-20 16:27:07 +0300

    Better, more customizable home and spawners scripts

--- Assets/_Root/Scripts/TowerDefense/Core/Stage/Home.cs
+++ Assets/_Root/Scripts/TowerDefense/Core/Stage/Home.cs
@@ -1,8 +1,22 @@
 ﻿namespace Kpi.Intro2GameDev.TowerDefense.Core.Stage
 {
+    using Enemies;
+    using JetBrains.Annotations;
     using UnityEngine;
     public class Home : MonoBehaviour
     {
-        
+        public Vector3 RandomGoal => transform.position + Vector3.right * Random.Range(-transform.localScale.x / 2, transform.localScale.x / 2);
+
+        [UsedImplicitly]
+        private void OnTriggerEnter(Collider other)
+        {
+            var zombie = other.gameObject.GetComponent<Enemy>();
+            if (zombie != null)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+
+
     }
 }
--- Assets/_Root/Scripts/TowerDefense/Core/Stage/Spawner.cs
+++ Assets/_Root/Scripts/TowerDefense/Core/Stage/Spawner.cs
@@ -9,15 +9,34 @@
         [SerializeField, EditorAssigned]
         private Enemy prefab;
 
+        [SerializeField, EditorAssigned, Range(1, 20)]
+        private float width;
+
+        [SerializeField, EditorAssigned, Range(1, 30)]
+        private float delayScatter = 10;
+
+        [SerializeField, EditorAssigned, Range(0, 30)]
+        private float delay = 4;
+
+        private Transform enemiesParent;
+
         [UsedImplicitly]
         public void Start()
         {
+            enemiesParent = GameObject.Find("Enemies").transform;
             StartCoroutine(SpawnSequence());
+        }
+
+
+        [UsedImplicitly]
+        private void OnDrawGizmos()
+        {
+           DrawCube(transform.position, transform.rotation, new Vector3(width, 2, 2));
         }
 
         private void Spawn()
         {
-            Instantiate(prefab, transform.position, Quaternion.identity);
+            Instantiate(prefab, transform.position + Vector3.right * Random.Range(-width/2, width/2), Quaternion.identity, enemiesParent);
         }
 
         private IEnumerator SpawnSequence()
@@ -25,8 +44,21 @@
             while (true)
             {
                 Spawn();
-                yield return new WaitForSeconds(Random.value * 10 + 4);
+                yield return new WaitForSeconds(Random.value * delayScatter + delay);
             }
+        }
+
+        public static void DrawCube(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            Matrix4x4 cubeTransform = Matrix4x4.TRS(position, rotation, scale);
+            Matrix4x4 oldGizmosMatrix = Gizmos.matrix;
+
+            Gizmos.color = Color.green;
+            Gizmos.matrix *= cubeTransform;
+
+            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+
+            Gizmos.matrix = oldGizmosMatrix;
         }
     }
 }
