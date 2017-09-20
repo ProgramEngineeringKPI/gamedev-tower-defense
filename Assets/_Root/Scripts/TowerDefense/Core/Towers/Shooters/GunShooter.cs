2695907788a58a37c823ef8f37a1dfc027f1e5ee
artem.korotenko <artem.korotenko@tatemgames.com>
2017-09-20 18:42:31 +0300

    Zombies shooting from Tower

---
+++ Assets/_Root/Scripts/TowerDefense/Core/Towers/Shooters/GunShooter.cs
@@ -0,0 +1,27 @@
+﻿namespace Kpi.Intro2GameDev.TowerDefense.Core.Towers.Shooters
+{
+    using JetBrains.Annotations;
+    using UnityEngine;
+
+    public class GunShooter : Shooter
+    {
+        [SerializeField, EditorAssigned]
+        private Bullet bulletPrefab;
+
+        [SerializeField, EditorAssigned]
+        private Transform muzzle;
+
+        [SerializeField, EditorAssigned]
+        private GameObject weapon;
+
+        protected override void Shot()
+        {
+            var aimOffset = weapon.transform.position - muzzle.position;
+            var aimPosition = Enemy.transform.position + aimOffset + Vector3.up * 2;
+            weapon.transform.rotation = Quaternion.LookRotation(aimPosition - weapon.transform.position);
+
+            var bullet = Instantiate(bulletPrefab, muzzle, false);
+            bullet.Shoot(muzzle.position, muzzle.rotation, muzzle.forward);
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/TowerDefense/Core/Towers/Tower.cs
@@ -0,0 +1,108 @@
+﻿namespace Kpi.Intro2GameDev.TowerDefense.Core.Towers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Enemies;
+    using JetBrains.Annotations;
+    using UnityEngine;
+
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(SphereCollider))]
+    public class Tower : MonoBehaviour
+    {
+        [SerializeField, EditorAssigned, Range(2, 40)]
+        private float radius = 30;
+
+        private new SphereCollider collider;
+
+        private readonly List<Enemy> enemies = new List<Enemy>();
+
+        private Shooter shooter;
+
+        private Enemy currentGoal;
+
+        [UsedImplicitly]
+        public void Start()
+        {
+            collider = GetComponent<SphereCollider>();
+            shooter = GetComponentInChildren<Shooter>();
+        }
+
+        [UsedImplicitly]
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position + Vector3.up * 0.5f, radius);
+            
+        }
+
+#if UNITY_EDITOR
+        [UsedImplicitly]
+        public void Update()
+        {
+            if (collider.radius.Equals(radius))
+            {
+                return;
+            }
+
+            collider.radius = radius;
+        }
+#endif
+
+        private void TryChooseNextGoal()
+        {
+            if (!enemies.Any())
+            {
+                return;
+            }
+
+            shooter.Reset();
+            currentGoal = enemies[Random.Range(0, enemies.Count)];
+            shooter.ShootAt(currentGoal);
+        }
+
+        [UsedImplicitly]
+        private void OnTriggerEnter(Collider other)
+        {
+            var enemy = other.gameObject.GetComponent<Enemy>();
+            if (enemy == null || enemies.Contains(enemy))
+            {
+                return;
+            }
+
+            enemies.Add(enemy);
+            enemy.Died += RemoveEnemy;
+
+            if (currentGoal == null)
+            {
+                TryChooseNextGoal();
+            }
+        }
+
+        [UsedImplicitly]
+        private void OnTriggerExit(Collider other)
+        {
+            var enemy = other.gameObject.GetComponent<Enemy>();
+            if (enemy == null)
+            {
+                return;
+            }
+
+            RemoveEnemy(enemy);
+        }
+
+        private void RemoveEnemy(Enemy enemy)
+        {
+            enemies.Remove(enemy);
+            enemy.Died -= RemoveEnemy;
+
+            if (currentGoal == enemy)
+            {
+                currentGoal = null;
+                TryChooseNextGoal();
+            }
+        }
+
+
+
+    }
+}
