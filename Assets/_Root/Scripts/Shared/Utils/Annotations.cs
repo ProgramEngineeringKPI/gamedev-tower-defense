745aaf2307eeba1a8922a3bbbd695ba370a9cdce
artem.korotenko <artem.korotenko@tatemgames.com>
2017-09-20 15:42:22 +0300

    Basic zombie spawn, navigation

---
+++ Assets/_Root/Scripts/Shared/Utils/Annotations.cs
@@ -0,0 +1,50 @@
+﻿#pragma warning disable 1591
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedParameter.Local
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable IntroduceOptionalParameters.Global
+// ReSharper disable MemberCanBeProtected.Global
+// ReSharper disable InconsistentNaming
+namespace JetBrains.Annotations
+{
+    using System;
+
+    /// <summary>
+    /// This attribute is intended to mark publicly available API
+    /// which should not be removed and so is treated as used
+    /// </summary>
+    [MeansImplicitUse(ImplicitUseKindFlags.Assign)]
+    public sealed class EditorAssignedAttribute : Attribute
+    {
+        public EditorAssignedAttribute()
+        {
+        }
+        
+         
+
+        public EditorAssignedAttribute([NotNull] string comment)
+        {
+            this.Comment = comment;
+        }
+
+        [NotNull]
+        public string Comment { get; private set; }
+    }
+
+    [MeansImplicitUse(ImplicitUseKindFlags.Assign)]
+    public sealed class JsonDeserializedAttribute : Attribute
+    {
+        public JsonDeserializedAttribute()
+        {
+        }
+        
+        public JsonDeserializedAttribute([NotNull] string comment)
+        {
+            this.Comment = comment;
+        }
+
+        [NotNull]
+        public string Comment { get; private set; }
+    }
+}
---
+++ Assets/_Root/Scripts/TowerDefense/Core/Stage/Spawner.cs
@@ -0,0 +1,32 @@
+﻿namespace Kpi.Intro2GameDev.TowerDefense.Core.Stage
+{
+    using System.Collections;
+    using Enemies;
+    using JetBrains.Annotations;
+    using UnityEngine;
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField, EditorAssigned]
+        private Enemy prefab;
+
+        [UsedImplicitly]
+        public void Start()
+        {
+            StartCoroutine(SpawnSequence());
+        }
+
+        private void Spawn()
+        {
+            Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+
+        private IEnumerator SpawnSequence()
+        {
+            while (true)
+            {
+                Spawn();
+                yield return new WaitForSeconds(Random.value * 10 + 4);
+            }
+        }
+    }
+}
