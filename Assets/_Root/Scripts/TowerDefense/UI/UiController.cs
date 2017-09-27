bd3c0b64061099f8ef377aa6b455b428e61188a3
Caritas <korotenkofm@gmail.com>
2017-09-27 19:50:23 +0300

    You Lose window

---
+++ Assets/_Root/Scripts/TowerDefense/UI/UiController.cs
@@ -0,0 +1,41 @@
+﻿namespace Kpi.Intro2GameDev.Assets.TowerDefense.UI
+{
+    using System;
+    using DG.Tweening;
+    using JetBrains.Annotations;
+    using strange.extensions.mediation.impl;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using _Root.Scripts.TowerDefense.Strange.Signals;
+
+    public class UiController : View
+    {
+        [Inject]
+        public PlayerLost PlayerLost { get; set; }
+
+        [SerializeField, EditorAssigned]
+        private GameObject LoseWindow;
+
+        [SerializeField, EditorAssigned]
+        private CanvasGroup back;
+
+        protected override void Start()
+        {
+            base.Start();
+            PlayerLost.AddListener(OnPlayerLost);
+        }
+
+        private void OnPlayerLost()
+        {
+            back.gameObject.SetActive(true);
+            back.alpha = 0;
+            back.DOFade(1f, 1.2f);
+
+            DOVirtual.DelayedCall(0.6f, () =>
+            {
+                LoseWindow.SetActive(true);
+                LoseWindow.transform.DOScale(Vector3.zero, 0.5f).From().SetEase(Ease.OutBack, 1.5f);
+            });
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/TowerDefense/Utils/HotKeyListener.cs
@@ -0,0 +1,22 @@
+﻿namespace Kpi.Intro2GameDev.Assets.TowerDefense.Utils
+{
+    using JetBrains.Annotations;
+    using strange.extensions.mediation.impl;
+    using UnityEngine;
+    using _Root.Scripts.TowerDefense.Strange.Signals;
+
+    public class HotKeyListener : View
+    {
+        [Inject]
+        public PlayerLost PlayerLost { get; set; }
+
+        [UsedImplicitly]
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PlayerLost.Dispatch();
+            }
+        }
+    }
+}
